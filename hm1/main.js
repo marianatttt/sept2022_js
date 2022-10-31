ae6b4cd0e1b29255a0be218c0f00535c2761baa1
marianatttt <malunka221988@gmail.com>
2022-11-01 01:06:40 +0200

    1

---
+++ hm1/main.js
@@ -0,0 +1,62 @@
+let p = 'hello';
+console.log(p);
+
+let p1 = 'owu';
+console.log(p1);
+
+let p2 = 'com';
+console.log(p2);
+
+let p3 = 'ua';
+console.log(p3);
+
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num2);
+
+let num3 = -999;
+console.log(num3);
+
+let num4 = 123;
+console.log(num4);
+
+
+const PI = 3.14;
+console.log(PI);
+
+const E = 2.7;
+console.log(E);
+
+let num5 = 16;
+console.log(num5);
+
+let boolean1 = true;
+console.log(boolean1);
+
+let boolean2 = false;
+console.log(boolean2);
+
+
+
+let a = 100;
+console.log(typeof a, a);
+
+let b = '100';
+console.log(typeof b, b);
+
+let c = true;
+console.log(typeof c, c)
+
+
+
+let lastname = 'Malynovska';
+let firstname = 'Mariana';
+let surname = 'Tarasivna';
+
+let person = lastname + ' ' + firstname + ' ' + surname;
+console.log(person);
+
+let person2 = `${lastname} ${firstname} ${surname}`;
+console.log(person2)
