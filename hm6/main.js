97608d0767455390a9c6c7aab85b67f35fff0f24
marianatttt <malunka221988@gmail.com>
2022-11-22 14:47:56 +0200

    //.

---
+++ hm6/main.js
@@ -0,0 +1,182 @@
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+let str1 = 'hello world';
+let str2 = 'lorem ipsum';
+let str3 = 'javascript is cool';
+
+console.log(str1["length"]);
+console.log(str2["length"]);
+console.log(str3["length"]);
+
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+console.log(str1.toUpperCase());
+console.log(str2.toUpperCase());
+console.log(str3.toUpperCase());
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+console.log(str1.toLowerCase());
+console.log(str2.toLowerCase());
+console.log(str3.toLowerCase());
+
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+let str = ' dirty string   ';
+console.log(str.trim())
+
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+
+let str5 = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+
+console.log(str5.split(' '))
+
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+
+let arr = [10,8,-7,55,987,-1011,0,1050,0];
+let newArr = arr.map(number => number + '' );
+console.log(newArr)
+
+
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+    let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+
+let sortNums = nums.sort((a,b) => { return a - b })
+
+console.log(sortNums);
+
+let sortNums1 = nums.sort((a,b) => {
+    if (a<b) {
+        return 1;
+    }
+    if (a>b) {
+        return -1;
+    }
+})
+
+console.log(sortNums1)
+
+
+// - є масив
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+// -- відсортувати його за спаданням за monthDuration
+
+let newArr3 = coursesAndDurationArray.sort((a, b) => {return b.monthDuration - a.monthDuration});
+console.log(newArr3);
+//
+// // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//
+let newArray = coursesAndDurationArray.filter(value => value.monthDuration>5)
+console.log(newArray)
+
+// описати колоду карт
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+
+let deck = [
+    {cardSuit:'Spade', value: 6, color:'Black'},
+    {cardSuit:'Clubs', value: 6, color:'Black'},
+    {cardSuit:'Hearts', value: 6, color:'Red'},
+    {cardSuit:'Diamonds', value: 6, color:'Red'},
+    {cardSuit:'Spade', value: 7, color:'Black'},
+    {cardSuit:'Clubs', value: 7, color:'Black'},
+    {cardSuit:'Hearts', value: 7, color:'Red'},
+    {cardSuit:'Diamonds', value: 7, color:'Red'},
+    {cardSuit:'Spade', value: 8, color:'Black'},
+    {cardSuit:'Clubs', value: 8, color:'Black'},
+    {cardSuit:'Hearts', value: 8, color:'Red'},
+    {cardSuit:'Diamonds', value: 8, color:'Red'},
+    {cardSuit:'Spade', value: 9, color:'Black'},
+    {cardSuit:'Clubs', value: 9, color:'Black'},
+    {cardSuit:'Hearts', value: 9, color:'Red'},
+    {cardSuit:'Diamonds', value: 9, color:'Red'},
+    {cardSuit:'Spade', value: 10, color:'Black'},
+    {cardSuit:'Clubs', value: 10, color:'Black'},
+    {cardSuit:'Hearts', value: 10, color:'Red'},
+    {cardSuit:'Diamonds', value: 10, color:'Red'},
+    {cardSuit:'Spade', value: 'Jack', color:'Black'},
+    {cardSuit:'Clubs', value: 'Jack', color:'Black'},
+    {cardSuit:'Hearts', value: 'Jack', color:'Red'},
+    {cardSuit:'Diamonds', value: 'Jack', color:'Red'},
+    {cardSuit:'Spade', value: 'Queen', color:'Black'},
+    {cardSuit:'Clubs', value: 'Queen', color:'Black'},
+    {cardSuit:'Hearts', value: 'Queen', color:'Red'},
+    {cardSuit:'Diamonds', value: 'Queen', color:'Red'},
+    {cardSuit:'Spade', value: 'King', color:'Black'},
+    {cardSuit:'Clubs', value: 'King', color:'Black'},
+    {cardSuit:'Hearts', value: 'King', color:'Red'},
+    {cardSuit:'Diamonds', value: 'King', color:'Red'},
+    {cardSuit:'Spade', value: 'Ace', color:'Black'},
+    {cardSuit:'Clubs', value: 'Ace', color:'Black'},
+    {cardSuit:'Hearts', value: 'Ace', color:'Red'},
+    {cardSuit:'Diamonds', value: 'Ace', color:'Red'}
+];
+let spadeJoker = deck.find(value => value.cardSuit === 'Spade' && value.value === 'Jack' && value.color === "Black");
+console.log(spadeJoker)
+
+let cardNumb6 = deck.filter(value => value. value === 6);
+console.log(cardNumb6)
+
+let redCard = deck.filter (value => value.color === 'Red');
+console.log(redCard)
+
+let diamondCard = deck.filter (value => value.cardSuit === 'Diamonds');
+console.log(diamondCard)
+
+let blackCard = deck.filter (value => value.color === 'Black');
+console.log(blackCard.splice(6, 18));
+
+
+
+// Додатково по reduce
+// Взяти описану колоду карт, та за допомоги редюсу попакувати всі карти по "мастях" в об'єкт
+// {
+//     spades:[],
+//     diamonds:[],
+//     hearts:[],
+//     clubs:[]
+// }
+
+let reduce = deck.reduce((acum,iteam) => {
+
+    if (iteam.cardSuit === 'Spade') {
+        acum.spades.push(iteam);
+    } else if(iteam.cardSuit === 'Diamonds') {
+        acum.diamonds.push(iteam);
+    } else if(iteam.cardSuit === 'Hearts') {
+        acum.hearts.push(iteam);
+    }else if(iteam.cardSuit === 'Clubs') {
+        acum.clubs.push(iteam);
+    }
+    return acum;
+}, {spades:[], diamonds:[], hearts:[], clubs:[] });
+console.log(reduce)
+
+
+
+
+
+// let arrrr= [10,8,-7,55,987,-1011,0,1050,0]
+// let mas=arrrr.map( function(numb){ return alert(arrrr);} );
+// console.log(mas)
